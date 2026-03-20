069cd55b4f284ce1e0573347aed3e686d7536ea4
ShubhanshuOO7 <shubhanshu5320413@gmail.com>
2026-03-20 09:41:43 +0400

    fix_cors_issue

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -8,7 +8,7 @@ import cors from "cors"
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000","https://idea-flow-excalidraw-frontend.vercel.app"],
+    origin: ["http://localhost:3000"," https://idea-flow-excalidraw-frontend-jepg.vercel.app/"],
     credentials: true
 }));
 app.post("/signup",async(req,res)=>{
